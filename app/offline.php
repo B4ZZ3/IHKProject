6be8a6417883638f2679ccd70a0ea445118907c0
Bjarne <develop.christel@gmail.com>
2020-01-22 22:29:40 +0100

    add: servie worker to the pwa

---
+++ app/offline.php
@@ -0,0 +1,7 @@
+<?php include "templates/include/header.php" ?>
+<div class="container d-flex align-items-center justify-content-center flex-wrap flex-column" style="height:100%;">
+<h1>Kein Internet</h1>
+<p>Bitte sorgen Sie dafür das das Smartphone eine aktive Internetverbindung hat. Ohne Internet kann die Inventur leider nicht durchgeführt werden!</p>
+<button onclick="window.location.reload();" class="btn btn-outline-primary">Nochmal versuchen</button>
+</div>
+<?php include "templates/include/footer.php" ?>
